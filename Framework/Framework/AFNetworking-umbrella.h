e6decf9d4b7d56be3df1d9c137785d349534bc31
Matthew Purland <m.purland@gmail.com>
2016-05-10 13:41:34 -0700

    Added Carthage support

---
+++ Framework/Framework/AFNetworking-umbrella.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Framework.
+FOUNDATION_EXPORT double FrameworkVersionNumber;
+
+//! Project version string for Framework.
+FOUNDATION_EXPORT const unsigned char FrameworkVersionString[];
+
+#include <AFNetworking/AFNetworking.h>
+#include <AFNetworking/UIKit+AFNetworking.h>
